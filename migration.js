53a26cd3c6b08f13a061601324018ad92d8c6a41
Cameron Eagans <cweagans@Thoth.local>
2009-07-06 03:08:49 -0600

    -Reworked the settings pages a little bit. It's now stubbed out in migration.admin.inc -Removed an extra if(TRUE) statement -- performance improvement -Added migration_footer() function that outputs some HTML for controlling the Query logger   -When we log queries, we need to do it ONLY if migration_query_logger == TRUE -There's a menu item for a migration table list now   -this would be kind of like the permissions form in core. Basically a big list of database tables with checkboxes. If they are checked, they get monitored.

---
+++ migration.js
@@ -0,0 +1,17 @@
+if (Drupal.jsEnabled) {
+  $(document).ready(function() {
+    
+    $('#migration_start_monitor').click(function(){
+      $.getJSON('/admin/settings/migration/start_monitor', function(json) {
+        alert(json.response);
+      });
+    });
+    
+    $('#migration_stop_monitor').click(function(){
+      $.getJSON('/admin/settings/migration/stop_monitor', function(json) {
+        alert(json.response);
+      });
+    });
+    
+  })
+}
