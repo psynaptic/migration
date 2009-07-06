ff6405d8bf9b139f6e7c730a23f31e25376ff190
psynaptic <rich@freestylesystems.co.uk>
2009-07-06 10:52:41 +0100

    Whitespace cleanup.

--- migration.js
+++ migration.js
@@ -1,17 +1,14 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-    
     $('#migration_start_monitor').click(function(){
       $.getJSON('/admin/settings/migration/start_monitor', function(json) {
         alert(json.response);
       });
     });
-    
     $('#migration_stop_monitor').click(function(){
       $.getJSON('/admin/settings/migration/stop_monitor', function(json) {
         alert(json.response);
       });
     });
-    
   })
 }
